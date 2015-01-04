8ab7d8d6f3addd574edc233ff1a767012f23e176
BlackyFox <puissant@et.esiea.fr>
2015-01-04 18:52:50 +0100

    changement layout (nom et titres)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
 class Comment < ActiveRecord::Base
   belongs_to :game
+  validate :empty_or_not
+  
+  def empty_or_not
+    errors.add(:user_name, 'Le nom est obligatoire') if user_name.empty?
+    errors.add(:body, 'A quoi bon mettre un commentaire sans commentaire ?') if body.empty?
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,9 @@
 class Game < ActiveRecord::Base
   has_many :comments
   mount_uploader :picture, PictureUploader
+  validate :empty_or_not
+
+  def empty_or_not
+    errors.add(:name, 'Le titre est obligatoire') if name.empty?
+  end
 end
